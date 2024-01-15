ff1d6861f87eaa9f30efaa3338782b7b95f54140
Phạm Hà Anh <89311003+D-RubyIV@users.noreply.github.com>
2024-01-15 22:32:18 +0700

    update port

--- apps/backend/myapp/settings.py
+++ apps/backend/myapp/settings.py
@@ -64,6 +64,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000", 
     "http://127.0.0.1:8000",
     "http://127.0.0.1:3000",
+    "http://127.0.0.1:3000", 
+    "http://172.20.10.6:8000", 
 ]
 CORS_ALLOW_METHODS = (
     "DELETE",
